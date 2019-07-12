2e169fe490074ad1294b05c2aa068167180c91ad
Tyler Scott <tyduptyler13@gmail.com>
2019-07-12 15:59:37 -0600

    Fixed authentication issues

--- src/main/java/io/fusionauth/config/SecurityConfig.java
+++ src/main/java/io/fusionauth/config/SecurityConfig.java
@@ -1,16 +1,20 @@
 package io.fusionauth.config;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 
 /**
  * @author Tyler Scott
@@ -19,9 +23,14 @@ import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMap
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
   @Value("${fusionAuth.redirectUri}")
   private String redirectUri;
+
+  @Value("${fusionAuth.clientId}")
+  private String cliendId;
+
+  @Value("${fusionAuth.logoutUri}")
+  private String logoutUri;
 
   @Override
   public void configure(WebSecurity web) {
@@ -31,23 +40,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-        .antMatchers("/", "/error").permitAll()
+        .antMatchers("/", "/error", "/login**").permitAll()
         .anyRequest().permitAll()
         .and()
         .oauth2Login()
-        .loginPage("/login")
-        .redirectionEndpoint().baseUri(redirectUri)
-        .and()
         .userInfoEndpoint()
-        .userAuthoritiesMapper(userAuthoritiesMapper());
+        .userAuthoritiesMapper(userAuthoritiesMapper())
+        .and()
+        .defaultSuccessUrl("/profile");
+
+    if (!logoutUri.isEmpty()) {
+      http.logout()
+          .logoutSuccessUrl(logoutUri + "?client_id=" + cliendId); // If you set the logoutUri, people will be redirected here after its complete.
+      // In this case, we also append the client_id because if we redirect to FusionAuth this way, it can redirect us back after it logs us
+      // out globally.
+    }
   }
 
-  @Bean
-  protected GrantedAuthoritiesMapper userAuthoritiesMapper() {
-    return authorities -> {
-      authorities.forEach(authority -> System.out.println(authority.toString()));
+  private GrantedAuthoritiesMapper userAuthoritiesMapper() {
+    return authorities -> authorities.stream()
+                                     .flatMap(authority -> {
+                                       List<String> roles;
+                                       if (authority instanceof OidcUserAuthority) {
+                                         roles = (List<String>) ((OidcUserAuthority) authority).getIdToken().getClaims().getOrDefault("roles", Collections.emptyList());
+                                       } else if (authority instanceof OAuth2UserAuthority) {
+                                         roles = (List<String>) ((OAuth2UserAuthority) authority).getAttributes().getOrDefault("roles", Collections.emptyList());
+                                       } else {
+                                         roles = Collections.emptyList();
+                                       }
 
-      return Collections.emptyList(); // FIXME
-    };
+                                       return roles.stream();
+                                     })
+                                     .map(SimpleGrantedAuthority::new)
+                                     .collect(Collectors.toList());
   }
 }
--- src/main/java/io/fusionauth/controller/MasterControllerProgram.java
+++ src/main/java/io/fusionauth/controller/MasterControllerProgram.java
@@ -1,45 +1,27 @@
 package io.fusionauth.controller;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author Tyler Scott
  */
 @Controller
 public class MasterControllerProgram {
-  @RequestMapping(value = "/logout", method = RequestMethod.GET)
-  @PreAuthorize("permitAll()")
-  public RedirectView handleLogout(@Autowired HttpServletRequest request, @RequestParam Optional<String> redirect) throws ServletException {
-    request.logout();
-    if (redirect.isPresent() && redirect.get().length() != 0) {
-      return new RedirectView(redirect.get());
-    }
-    return new RedirectView("/");
-  }
-
-  @RequestMapping(value = "/admin", method = RequestMethod.GET)
+  @GetMapping(value = "/admin")
   @PreAuthorize("hasAuthority('admin')")
   public String viewAdmin() {
     return "admin";
   }
 
-  @RequestMapping(value = "/", method = RequestMethod.GET)
+  @GetMapping(value = "/")
   @PreAuthorize("permitAll()")
   public String viewHome() {
     return "hello";
   }
 
-  @RequestMapping(value = "/profile", method = RequestMethod.GET)
+  @GetMapping(value = "/profile")
   @PreAuthorize("hasAuthority('user') or hasAuthority('admin')")
   public String viewProfile() {
     return "profile";
